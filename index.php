bde5d19657a30e9684c9742602ffcd0b6da7e958
Alexandre Boeuf <alex.boeuf@gmail.com>
2020-12-05 20:01:26 +0100

    Typos

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   <h1>Advent Of Code : classement alternatif </h1>
   <p>
     Classement alternatif bas&eacute; sur le temps pass&eacute; entre la premi&egrave;re et la deuxi&egrave;me &eacute;toile.<br>
-    Chaque jour le premier prends N points, le deuxi&egrave;me N - 1 points, etc. (avec N le nombre de joueurs).<br>
-    Z&eacute;ro points tant que la deuxi&egrave;me &eacute;toile n'est pas obtenue.<br>
+    Chaque jour le premier remporte N points, le deuxi&egrave;me N - 1 points, etc. (avec N le nombre de joueurs).<br>
+    Aucun point tant que la deuxi&egrave;me &eacute;toile n'est pas obtenue.<br>
     En cas d'&eacute;galit&eacute; de points au g&eacute;n&eacute;ral, c'est la somme des d&eacute;lais qui d&eacute;partage.<br>
     Toutes les propositions de modifications sont les bienvenues.
   </p>
